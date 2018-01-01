dd3ccef33af1d066afa0e3b510c0042bab9680aa
Hidan428 <32068251+Hidan428@users.noreply.github.com>
2018-01-01 14:11:37 +0100

    'PremiereVersion'

---
+++ PlStream/Scripts/StartTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTrigger : MonoBehaviour {
+
+	private Timer time;
+	private AudioSource startAudio;
+
+	// Use this for initialization
+	void Awake () {
+		time = GameObject.Find("Canvas").GetComponent<Timer>();
+		startAudio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//Debug.Log(other.ClosestPoint);
+		if(time.count == false)
+		{
+			Debug.Log("GOOOO");
+	        startAudio.Play();
+	        time.time = 0;
+	        time.count = true;
+		}
+    }
+}
